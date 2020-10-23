9f9f6486bc8a29f1d2fe361caf4d301fe9dc598b
Andrew Dobis <andrew.dobis@epfl.ch>
2020-10-23 18:01:21 +0200

    added cross coverage

--- src/test/scala/coverage/CoverageDB.scala
+++ src/test/scala/coverage/CoverageDB.scala
@@ -3,7 +3,7 @@
 package coverage
 
 import chisel3.Data
-import coverage.Coverage.Bins
+import coverage.Coverage.{Bins, CoverPoint, Cross, CrossBin}
 
 import scala.collection.mutable
 import scala.collection.mutable.ArrayBuffer
@@ -19,17 +19,20 @@ class CoverageDB {
     //Contains a mapping for the bin ID pair (coverPointID, binID) to the current number of hits it has
     private val binIdNumHitsMap: mutable.HashMap[Bins, BigInt] = new mutable.HashMap[Bins, BigInt]()
 
+    //CrossBins
+    private val crossBinHitValuesMap: mutable.HashMap[CrossBin, List[(BigInt, BigInt)]] = new mutable.HashMap[CrossBin, List[(BigInt, BigInt)]]()
+    private val crossBinNumHitsMap: mutable.HashMap[CrossBin, BigInt] = new mutable.HashMap[CrossBin, BigInt]()
+
+    //Mappings for cross coverage
+    private val pointNameToPoint: mutable.HashMap[String, CoverPoint] = new mutable.HashMap[String, CoverPoint]()
+    private val crossToPoints: mutable.HashMap[Cross, (CoverPoint, CoverPoint)] = new  mutable.HashMap[Cross, (CoverPoint, CoverPoint)]()
+    private val pointToCross: mutable.HashMap[CoverPoint, Cross] = new mutable.HashMap[CoverPoint, Cross]()
+
     //Keep track of the different valid IDs
     private val groupIds: ArrayBuffer[BigInt] = new ArrayBuffer[BigInt]()
     private val pointIds: ArrayBuffer[BigInt] = new ArrayBuffer[BigInt]()
 
-    //Internal group->point->bin mappings
-    private val groupPointMap: mutable.HashMap[BigInt, List[BigInt]] = new mutable.HashMap[BigInt, List[BigInt]]()
-
-    //TODO: Maybe add some options
-
     private var lastCoverGroupId: BigInt = 0
-    private var lastCoverPointId: BigInt = 0
 
     /**
       * Adds a coverGroup to the list of valid coverGroups
@@ -41,35 +44,27 @@ class CoverageDB {
         lastCoverGroupId
     }
 
-    /**
-      * Creates a coverPoint entry for the given coverGroup
-      * @param groupId the id of the coverGroup containing the new coverPoint
-      * @param dutPortName the name of the port that's being sampled by the new coverPoint
-      * @return the newly generated id for the added coverPoint
-      */
-    def createCoverPoint(groupId: BigInt, dutPortName: String, dutPort: Data): BigInt = {
-        //Sanity check
-        if(groupIds contains groupId) {
-            //Update the current max id
-            lastCoverPointId += 1
-
-            //Add the mappings
-            val oldList = groupPointMap getOrElse(groupId, Nil)
-            groupPointMap update (groupId, oldList :+ lastCoverPointId)
-
-            coverIdPortMap update (dutPortName, dutPort)
-            pointIds append lastCoverPointId
-
-            //Return the coverPoint's id
-            lastCoverPointId
-        } else {
-            throw new IllegalArgumentException("Invalid CoverGroup ID!")
+    def registerCross(cross: Cross) : Unit = {
+        val point1 = pointNameToPoint get cross.pointName1 match {
+            case None => throw new IllegalArgumentException(s"${cross.pointName1} is not a registered coverpoint!")
+            case Some(p) => p
         }
+        val point2 = pointNameToPoint get cross.pointName2 match {
+            case None => throw new IllegalArgumentException(s"${cross.pointName2} is not a registered coverpoint!")
+            case Some(p) => p
+        }
+
+        //Register the cross
+        crossToPoints update (cross, (point1, point2))
+        pointToCross update (point1, cross)
+        pointToCross update (point2, cross)
     }
+
+    def getCrossFromPoint(point: CoverPoint) : Cross = pointToCross getOrElse(point, null)
+    def getPointsFromCross(cross: Cross) : (CoverPoint, CoverPoint) = crossToPoints getOrElse(cross, null)
 
     /**
       * Updates the number of hits done in a given bin
-      * @param binId the identifier of the bin we want to increment
       */
     def addBinHit(bin: Bins, value: BigInt): Unit = {
         val newValues = (binIdHitValuesMap.getOrElse(bin, Nil) :+ value).distinct
@@ -79,15 +74,28 @@ class CoverageDB {
     }
 
     /**
+      * Updates the number of hits done in a given bin
+      */
+    def addCrossBinHit(crossBin: CrossBin, value: (BigInt, BigInt)): Unit = {
+        val newValues = (crossBinHitValuesMap.getOrElse(crossBin, Nil) :+ value).distinct
+
+        crossBinHitValuesMap update (crossBin, newValues)
+        crossBinNumHitsMap update (crossBin, newValues.length)
+    }
+
+    def registerCoverPoint(name: String, coverPoint: CoverPoint) : Unit =
+        if(pointNameToPoint contains name) throw new IllegalArgumentException("CoverPoint Name already taken!")
+        else pointNameToPoint update (name, coverPoint)
+
+    /**
       * Gets the number of hits form the DB for a given bin id
-      * @param binId the unique identifier of the bin
       * @return the number of hits for the given bin
       */
     def getNHits(bin: Bins): BigInt = binIdNumHitsMap getOrElse (bin, 0)
+    def getNHits(cross: CrossBin): BigInt = crossBinNumHitsMap getOrElse (cross, 0)
 
     /**
       * Retrieves a port name given an id
-      * @param id the id of the coverPoint that's sampling a port
       * @return a string containing the name of the port
       */
     def getPort(portName: String): Data = coverIdPortMap get portName match {
