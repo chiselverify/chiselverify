afde30f993e0315355aa18008b19e10e189fa221
tjarker <61082372+tjarker@users.noreply.github.com>
2020-10-25 11:56:27 +0100

    updated microsemi use case
    implemented main tests with chiseltester
    included cross coverage

--- src/test/scala/heappriorityqueue/PriorityQueueTests.scala
+++ src/test/scala/heappriorityqueue/PriorityQueueTests.scala
@@ -1,125 +1,137 @@
 package heappriorityqueue
 
-import org.scalatest.{FlatSpec, Matchers}
+import org.scalatest._
+import chiseltest._
+import coverage.Coverage.{Bins, CoverPoint, Cross, CrossBin}
+import coverage.CoverageReporter
 
-/**
- * contains 2 test plans for the heap based priority queue
- *  - one drives a single random test onto the dut
- *  - the other tests all memory size and children count configurations
- */
-class PriorityQueueTests extends FlatSpec with Matchers {
+class PriorityQueueCoverageTest extends FreeSpec with ChiselScalatestTester {
+
   val cWid = 2
   val nWid = 8
-  val rWid = 5
-  val heapSize = 17
-  val chCount = 4
-  val debugLvl = 0
-  val testRuns = 10
-  "HeapPriorityQueue" should s"pass $testRuns test runs" in {
-    chisel3.iotesters.Driver(() => new HeapPriorityQueue(heapSize, chCount, nWid, cWid, rWid)) {
-      c => {new PriorityQueueTester(testRuns,debugLvl)(c,heapSize,chCount)(cWid,nWid,rWid).dut}
-    } should be(true)
+  val rWid = 3
+
+  def tester(c: HeapPriorityQueue, heapSize: Int, chCount: Int, debugLvl: Int, testRuns: Int) : Unit = {
+    var stepCounter = 0
+    var successfulInsertions = 0
+    var successfulRemovals = 0
+    var insertionTimes = 0
+    var removalTimes = 0
+    var refIDcounter = 0
+
+    val rand = scala.util.Random
+
+    val dut = new HeapPriorityQueueWrapper(c, heapSize, chCount, debugLvl)(cWid, nWid, rWid)
+    val model = new Behavioural(heapSize, chCount)(cWid, nWid, rWid)
+
+    ////////////////////////////////////////////////////Coverage////////////////////////////////////////////////////////
+
+    val cr = new CoverageReporter
+    cr.register(
+      CoverPoint(c.io.cmd.op, "operation",
+        Bins("insertion", 0 to 0)::Bins("removal", 1 to 1)::Nil)::
+        CoverPoint(c.io.cmd.prio.cycl, "cmd.prio.cycl",
+          Bins("cyclic", 0 to 3)::Nil)::
+        CoverPoint(c.io.cmd.prio.norm, "cmd.prio.norm",
+          Bins("lower half", 0 to (Math.pow(2,nWid)/2-1).toInt)::Bins("upper half", (Math.pow(2,nWid)/2-1).toInt to (Math.pow(2,nWid)-1).toInt)::Nil)::
+        CoverPoint(c.io.head.prio.cycl, "head.prio.cycl",
+          Bins("cyclic", 0 to 3)::Nil)::
+        CoverPoint(c.io.head.prio.norm, "head.prio.norm",
+          Bins("lower half", 0 to (Math.pow(2,nWid)/2-1).toInt)::Bins("upper half", (Math.pow(2,nWid)/2-1).toInt to (Math.pow(2,nWid)-1).toInt)::Nil)::
+
+        Nil,
+      //Declare cross points
+      Cross("cyclics at ops", "operation", "cmd.prio.cycl",
+        CrossBin("insertion", 0 to 0, 0 to 3)::CrossBin("removal", 1 to 1, 0 to 3)::Nil)::
+        Cross("normals at ops", "operation", "cmd.prio.norm",
+          CrossBin("insertion lower half", 0 to 0, 0 to (Math.pow(2,nWid)/2-1).toInt)::CrossBin("insertion upper half", 0 to 0, (Math.pow(2,nWid)/2-1).toInt to (Math.pow(2,nWid)-1).toInt)::
+            CrossBin("removal lower half", 1 to 1, 0 to (Math.pow(2,nWid)/2-1).toInt)::CrossBin("removal upper half", 1 to 1, (Math.pow(2,nWid)/2-1).toInt to (Math.pow(2,nWid)-1).toInt)::Nil)::
+        Nil)
+
+    ////////////////////////////////////////////////helper functions////////////////////////////////////////////////////
+
+    def randomPoke() : Seq[Int] = {
+      // op, cprio, nprio, refID
+      val ret = Seq(rand.nextInt(2),rand.nextInt(math.pow(2, cWid).toInt), rand.nextInt(math.pow(2, nWid).toInt), refIDcounter)
+      refIDcounter += 1
+      if (refIDcounter >= Math.pow(2, rWid)-1) refIDcounter = 0
+      ret
+    }
+
+    def test(values: Seq[Seq[Int]]) : Unit = {
+      for (poke <- values) {
+        var debug = ""
+        if (poke.head==0) { // insert operation
+          // simulate dut and model
+          val (steps, success, debugStr) = dut.insert(poke(1), poke(2), poke(3))
+          model.insert(poke(1), poke(2), poke(3))
+
+          // process results
+          debug = debugStr
+          stepCounter += steps
+          if (success) {
+            insertionTimes += steps
+            successfulInsertions += 1
+          }
+        } else { // remove operation
+          // simulate dut and model
+          val (steps, success, debugStr) = dut.remove(poke(3))
+          model.remove(poke(3))
+
+          // process results
+          debug = debugStr
+          stepCounter += steps
+          if (success) {
+            removalTimes += steps
+            successfulRemovals += 1
+          }
+        }
+        // cross check dut and model; kill test and print debug if not matching
+        cr.sample()
+        if (!dut.compareWithModel(model.mem)) {
+          println(debug)
+          sys.exit(0)
+        }
+      }
+    }
+
+    //////////////////////////////////////////////////////testing/////////////////////////////////////////////////////////
+
+    val pokes = Seq.fill(testRuns)(randomPoke())
+
+    test(pokes)
+
+    ///////////////////////////////////////////////////reports//////////////////////////////////////////////////////////
+
+    val avgIns = insertionTimes.toDouble/successfulInsertions
+    val avgRem = removalTimes.toDouble/successfulRemovals
+    val successRate = ((successfulInsertions+successfulRemovals.toDouble)/testRuns)*100
+
+    println(s"${"="*20}Report${"="*20}\n"+
+      s"Heapsize = $heapSize, children count = $chCount\n"+
+      s"${"%.2f".format(successRate)}% of operations were valid\n"+
+      s"$successfulInsertions insertions which took on average ${"%.2f".format(avgIns)} cycles\n"+
+      s"$successfulRemovals removals which took on average ${"%.2f".format(avgRem)} cycles\n"+
+      s"${"="*46}"
+    )
+
+    cr.printReport()
   }
-  "HeapPriorityQueue" should s"pass 16*$testRuns test runs with different memory sizes and children count" in {
+
+  /////////////////////////////////////////////////////tests////////////////////////////////////////////////////////////
+
+  "HeapPriorityQueue pass a single random poke test run" in {
+    val heapSize = 17
+    val chCount = 4
+    test(new HeapPriorityQueue(heapSize,chCount,nWid,cWid,rWid)) { c => tester(c,heapSize,chCount,0,100)}
+  }
+
+  "HeapPriorityQueue should pass random poke test runs with different memory sizes and children count" in {
     val sizes = Array(33,65,129,257)
     val chCounts = Array(2,4,8,16)
-    for(size <- sizes) {
-      for(chCount <- chCounts) {
-        chisel3.iotesters.Driver(() => new HeapPriorityQueue(size, chCount, nWid, cWid, rWid)) {
-          c => {
-            new PriorityQueueTester(testRuns, debugLvl)(c, size, chCount)(cWid, nWid, rWid).dut
-          }
-        } should be(true)
-      }
-    }
-  }
-}
-
-/**
- * Tester class containing the input driving code for the model and the dut
- *  - the dut and model are poked with random operations and values
- *  - after every operation the memory state of the dut is verified by comparing with the behavioural model
- * @param testRuns number of operations to poke the dut with
- * @param debugLvl debug details: 0=muted, 1=overall information, 2=stepwise information
- * @param c the dut
- * @param heapSize the maximum size of the heap
- * @param chCount the number of children per node in the heap
- * @param cWid the width of the cyclic priority field
- * @param nWid the width of the normal priority field
- * @param rWid the width of the reference ID field
- */
-class PriorityQueueTester(testRuns: Int, debugLvl: Int)(c: HeapPriorityQueue, heapSize: Int, chCount: Int)(cWid : Int,nWid : Int,rWid : Int) {
-
-  val rand = scala.util.Random
-  val dut = new HeapPriorityQueueWrapper(c, heapSize, chCount, debugLvl)(cWid, nWid, rWid)
-  val model = new Behavioural(heapSize, chCount)(cWid, nWid, rWid)
-
-  var refIDcounter = 0
-
-  // tracking variables
-  var stepCounter = 0
-  var successfulInsertions = 0
-  var successfulRemovals = 0
-  var insertionTimes = 0
-  var removalTimes = 0
-
-  for (i <- 0 until testRuns) {
-    var debug = ""
-    if (rand.nextInt(2) == 1) { // insert operation
-      // determine what to insert
-      val ins = Array(rand.nextInt(math.pow(2, cWid).toInt), rand.nextInt(math.pow(2, nWid).toInt), refIDcounter)
-      // increment reference ID counter and wrap if necessary
-      refIDcounter += 1
-      if (refIDcounter >= Math.pow(2, rWid)) refIDcounter = 0
-
-      // simulate dut and model
-      val (steps,success,debugStr) = dut.insert(ins(0), ins(1), ins(2))
-      model.insert(ins(0), ins(1), ins(2))
-
-      // process results
-      debug = debugStr
-      stepCounter += steps
-      if(success){
-        insertionTimes += steps
-        successfulInsertions += 1
-      }
-    } else { // remove operation
-      // determine removal reference ID
-      val rem = rand.nextInt(math.pow(2, rWid).toInt - 1)
-
-      // simulate dut and model
-      val (steps,success,debugStr) = dut.remove(rem)
-      model.remove(rem)
-
-      // process results
-      debug = debugStr
-      stepCounter += steps
-      if(success){
-        removalTimes += steps
-        successfulRemovals += 1
-      }
-    }
-    // cross check dut and model; kill test and print debug if not matching
-    if(!dut.compareWithModel(model.mem)){
-      println(debug)
-      sys.exit(0)
-    }
+    sizes.foreach(heapSize => chCounts.foreach(chCount => {
+      test(new HeapPriorityQueue(heapSize,chCount,nWid,cWid,rWid)) { c => tester(c,heapSize,chCount,0,100)}
+    }))
   }
 
-  //////////////////////////////////////////////////Report////////////////////////////////////////////////////////
-
-  val avgIns = insertionTimes.toDouble/successfulInsertions
-  val avgRem = removalTimes.toDouble/successfulRemovals
-  val successRate = ((successfulInsertions+successfulRemovals.toDouble)/testRuns)*100
-
-  println(s"${"="*20}Report${"="*20}\n"+
-    s"Heapsize = $heapSize, children count = $chCount\n"+
-    s"${"%.2f".format(successRate)}% of operations were successful\n"+
-    s"$successfulInsertions insertions which took on average ${"%.2f".format(avgIns)} cycles\n"+
-    s"$successfulRemovals removals which took on average ${"%.2f".format(avgRem)} cycles\n"+
-    s"${"="*46}"
-  )
-
-  ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 }
-
