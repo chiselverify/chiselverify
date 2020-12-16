8a69b708e1674af32ef17178b109295d8853f38a
Dobios <andre.dobis@gmail.com>
2020-12-16 14:21:33 +0100

    added joins in tests

--- src/test/scala/verifyTests/assertions/TimedAssertionTests.scala
+++ src/test/scala/verifyTests/assertions/TimedAssertionTests.scala
@@ -4,9 +4,7 @@ import chisel3._
 import chisel3.tester._
 import chiseltest.ChiselScalatestTester
 import chiselverify.assertions.AssertTimed
-import chiselverify.timing.Delay._
-import chiselverify.timing.Event
-import chiselverify.timing.Event._
+import chiselverify.timing._
 import org.scalatest.{FlatSpec, Matchers}
 import verifyTests.ToyDUT.AssertionsToyDUT
 
@@ -24,26 +22,26 @@ class TimedAssertionTests extends FlatSpec with ChiselScalatestTester with Match
         def testAlways(): Unit = {
             dut.io.a.poke(10.U)
             dut.io.b.poke(10.U)
-            AssertTimed(dut, () => (dut.io.aEqb === 1.U).litToBoolean, "aEqb timing is wrong")(Always(10))
+            AssertTimed(dut, () => (dut.io.aEqb === 1.U).litToBoolean, "aEqb timing is wrong")(Always(10)).join()
         }
 
         def testAlwaysFail(): Unit = {
             dut.io.a.poke(10.U)
             dut.io.b.poke(10.U)
-            assertThrows[AssertionError](AssertTimed(dut, () => (dut.io.aEqb === 1.U).litToBoolean, "aEqb timing is wrong")(Always(15)))
+            assertThrows[AssertionError](AssertTimed(dut, () => (dut.io.aEqb === 1.U).litToBoolean, "aEqb timing is wrong")(Always(15)).join())
         }
 
         def testEventually(): Unit = {
             dut.io.a.poke(10.U)
             dut.io.b.poke(10.U)
-            AssertTimed(dut, () => (dut.io.aEvEqC === 1.U).litToBoolean, "a eventually is c")(Eventually(11))
+            AssertTimed(dut, () => (dut.io.aEvEqC === 1.U).litToBoolean, "a eventually is c")(Eventually(11)).join()
         }
 
         et match {
-            case Event.Always =>
+            case Always =>
                 testAlways()
                 testAlwaysFail()
-            case Event.Eventually => testEventually()
+            case Eventually => testEventually()
         }
         testAlways()
         testEventually()
@@ -52,8 +50,8 @@ class TimedAssertionTests extends FlatSpec with ChiselScalatestTester with Match
 
     "Timed Assertions" should "pass" in {
         test(new AssertionsToyDUT(32)){ dut => {
-            testGeneric(dut, Event.Always)
-            testGeneric(dut, Event.Eventually)
+            testGeneric(dut, Always)
+            testGeneric(dut, Eventually)
         } }
     }
 }
