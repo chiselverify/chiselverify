a3456f078ad2db6427d51ccec86343b809923c56
hansemandse <hansjakobdamsgaard@gmail.com>
2021-01-08 14:57:46 +0100

    move axi4 from class2020 -> chiselverify

---
+++ src/main/scala/chiselverify/axi4/Interfaces.scala
@@ -0,0 +1,108 @@
+/*
+* Copyright 2020 DTU Compute - Section for Embedded Systems Engineering
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+package chiselverify.axi4
+
+import chisel3._
+import chisel3.util.Decoupled
+
+/** AXI4-Lite master interface
+ * 
+ * @param addrW the width of the address signals in bits
+ * @param dataW the width of the data read/write signals in bits
+ */
+class MasterInterfaceLite(val addrW: Int, val dataW: Int) extends Bundle {
+  /** Fields implementing each of the AXI channels
+   * 
+   * [[wa]] is the write address channel
+   * [[wd]] is the write data channel
+   * [[wr]] is the write response channel
+   * [[ra]] is the read address channel
+   * [[rd]] is the read data channel
+   */
+  val wa = Decoupled(Output(WALite(addrW)))
+  val wd = Decoupled(Output(WDLite(dataW)))
+  val wr = Flipped(Decoupled(Output(WRLite())))
+  val ra = Decoupled(RALite(addrW))
+  val rd = Flipped(Decoupled(Output(RDLite(dataW))))
+}
+
+/** AXI4 full master interface
+ * 
+ * @param addrW the width of the address signals in bits
+ * @param dataW the width of the data read/write signals in bits
+ * @param idW the width of the ID signals in bits, defaults to 0
+ * @param userW the width of the user signals in bits, defaults to 0
+ */
+class MasterInterface(override val addrW: Int, override val dataW: Int, val idW: Int = 0, val userW: Int = 0) extends MasterInterfaceLite(addrW, dataW) {
+  /** Fields implementing each of the AXI channels
+   * 
+   * [[wa]] is the write address channel
+   * [[wd]] is the write data channel
+   * [[wr]] is the write response channel
+   * [[ra]] is the read address channel
+   * [[rd]] is the read data channel
+   */
+  override val wa = Decoupled(Output(WA(addrW, idW, userW)))
+  override val wd = Decoupled(Output(WD(dataW, userW)))
+  override val wr = Flipped(Decoupled(Output(WR(idW, userW))))
+  override val ra = Decoupled(RA(addrW, idW, userW))
+  override val rd = Flipped(Decoupled(Output(RD(dataW, idW, userW))))
+}
+
+/** AXI4-Lite slave interface
+ * 
+ * @param addrW the width of the address signals in bits
+ * @param dataW the width of the data read/write signals in bits
+ */
+class SlaveInterfaceLite(val addrW: Int, val dataW: Int) extends Bundle {  
+  /** Fields implementing each of the AXI channels
+   * 
+   * [[wa]] is the write address channel
+   * [[wd]] is the write data channel
+   * [[wr]] is the write response channel
+   * [[ra]] is the read address channel
+   * [[rd]] is the read data channel
+   */
+  val wa = Flipped(Decoupled(Output(WALite(addrW))))
+  val wd = Flipped(Decoupled(Output(WDLite(dataW))))
+  val wr = Decoupled(Output(WRLite()))
+  val ra = Flipped(Decoupled(Output(RALite(addrW))))
+  val rd = Decoupled(Output(RDLite(dataW)))
+}
+
+/** AXI4 full slave interface
+ * 
+ * @param addrW the width of the address signals in bits
+ * @param dataW the width of the data read/write signals in bits
+ * @param idW the width of the ID signals in bits, defaults to 0
+ * @param userW the width of the user signals in bits, defaults to 0
+ */
+class SlaveInterface(override val addrW: Int, override val dataW: Int, val idW: Int = 0, val userW: Int = 0) extends SlaveInterfaceLite(addrW, dataW) {  
+  /** Fields implementing each of the AXI channels
+   * 
+   * [[wa]] is the write address channel
+   * [[wd]] is the write data channel
+   * [[wr]] is the write response channel
+   * [[ra]] is the read address channel
+   * [[rd]] is the read data channel
+   */
+  override val wa = Flipped(Decoupled(Output(WA(addrW, idW, userW))))
+  override val wd = Flipped(Decoupled(Output(WD(dataW, userW))))
+  override val wr = Decoupled(Output(WR(idW, userW)))
+  override val ra = Flipped(Decoupled(Output(RA(addrW, idW, userW))))
+  override val rd = Decoupled(Output(RD(dataW, idW, userW)))
+}
---
+++ src/main/scala/chiselverify/axi4/package.scala
@@ -0,0 +1,118 @@
+/*
+* Copyright 2020 DTU Compute - Section for Embedded Systems Engineering
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+* http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+* or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+*/
+
+package chiselverify
+
+import chisel3._
+
+package object axi4 {
+  /** AXI4-Lite master
+   * 
+   * An empty class representing an AXI master
+   *
+   * @param addrW the width of the address signals in bits
+   * @param dataW the width of the data read/write signals in bits
+   */
+  abstract class LiteMaster(val addrW: Int, val dataW: Int) extends Module {
+    val io = IO(new MasterInterfaceLite(addrW, dataW))
+  }
+
+  /** AXI4 full master
+   * 
+   * An empty class representing an AXI master
+   *
+   * @param addrW the width of the address signals in bits
+   * @param dataW the width of the data read/write signals in bits
+   * @param idW the width of the ID signals in bits, defaults to 0
+   * @param userW the width of the user signals in bits, defaults to 0
+   */
+  abstract class Master(val addrW: Int, val dataW: Int, val idW: Int = 0, val userW: Int = 0) extends Module {
+    val io = IO(new MasterInterface(addrW, dataW, idW, userW))
+  }
+
+  /** AXI4-Lite slave
+   * 
+   * An empty class representing an AXI slave
+   *
+   * @param addrW the width of the address signals in bits
+   * @param dataW the width of the data read/write signals in bits
+   */
+  abstract class LiteSlave(val addrW: Int, val dataW: Int) extends Module {
+    val io = IO(new SlaveInterfaceLite(addrW, dataW))
+  }
+
+  /** AXI4 full slave
+   * 
+   * An empty class representing an AXI slave
+   *
+   * @param addrW the width of the address signals in bits
+   * @param dataW the width of the data read/write signals in bits
+   * @param idW the width of the ID signals in bits, defaults to 0
+   * @param userW the width of the user signals in bits, defaults to 0
+   */
+  abstract class Slave(val addrW: Int, val dataW: Int, val idW: Int = 0, val userW: Int = 0) extends Module {
+    val io = IO(new SlaveInterface(addrW, dataW, idW, userW))
+  }
+
+  /** AXI4 burst encodings */
+  object BurstEncodings {
+    val Fixed             = "b00".U
+    val Incr              = "b01".U
+    val Wrap              = "b10".U
+  }
+  
+  /** AXI lock encodings */
+  object LockEncodings {
+    val NormalAccess     = false.B
+    val ExclusiveAccess  = true.B
+  }
+  
+  /** AXI4 memory encodings */
+  object MemoryEncodings {
+    val DeviceNonbuf     = "b0000".U
+    val DeviceBuf        = "b0001".U
+    val NormalNonbuf     = "b0010".U 
+    val NormalBuf        = "b0011".U
+    val WtNoalloc        = "b0110".U
+    val WtReadalloc      = "b0110".U
+    val WtWritealloc     = "b1110".U
+    val WtRwalloc        = "b1110".U
+    val WbNoalloc        = "b0111".U
+    val WbReadalloc      = "b0111".U
+    val WbWritealloc     = "b1111".U
+    val WbRwalloc        = "b1111".U
+  }
+  
+  /** AXI4 protection encodings */
+  object ProtectionEncodings {
+    val DataSecUpriv    = "b000".U
+    val DataSecPriv     = "b001".U
+    val DataNsecUpriv   = "b010".U
+    val DataNsecPriv    = "b011".U
+    val InstrSecUpriv   = "b100".U
+    val InstrSecPriv    = "b101".U
+    val InstrNsecUpriv  = "b110".U
+    val InstrNsecPriv   = "b111".U
+  }
+  
+  /** AXI4 response encodings */
+  object ResponseEncodings {
+    val Okay              = "b00".U
+    val Exokay            = "b01".U
+    val Slverr            = "b10".U
+    val Decerr            = "b11".U
+  }
+}
