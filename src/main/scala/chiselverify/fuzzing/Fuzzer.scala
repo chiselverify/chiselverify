53cbd798b68928fd7bf612bd83a76aa45bef4723
Andrew Dobis <andrew.dobis@epfl.ch>
2021-08-27 17:49:53 -0400

    WIP fuzz loop and added generic poke

--- src/main/scala/chiselverify/fuzzing/Fuzzer.scala
+++ src/main/scala/chiselverify/fuzzing/Fuzzer.scala
@@ -1,10 +1,13 @@
 package chiselverify.fuzzing
 
+import chisel3.Module.clock
 import chisel3._
 import chisel3.experimental.DataMirror
+import chisel3.tester.{testableClock, testableData}
 import chiselverify.coverage.CoverageReporter
 
 import java.nio.file.{Files, Paths}
+import java.io._
 
 object Fuzzer {
     def apply[T <: MultiIOModule](
@@ -18,22 +21,61 @@ object Fuzzer {
     ) : Int = {
         //Create a test corpus by reading seed file
         //Here we assume that the seeds have a legal format TODO: maybe bad idea?
-        val corpus = seeds.map(path => Files.readAllBytes(Paths.get(path)))
+        val seedCorpus = seeds.map(path => Files.readAllBytes(Paths.get(path))
+            .map(b => String.format("%8s", Integer.toBinaryString(b & 0xFF)))
+            .foldLeft("")((acc, byte) => acc ++ byte))
+
+        //Create a text corpus that will store all of our data during the fuzzing
+        val pw = new PrintWriter(new File("corpus.txt"))
+        pw.write(seedCorpus.foldLeft("")((acc, seed) => acc ++ seed ++ "\n"))
+        pw.close()
 
         //Get ports to poke
         val ports = (DataMirror.fullModulePorts(dut)).filter(p => p._1 != "clock" && p._1 != "reset" && p._1 != "io")
 
-        //TODO: Make a generic DUT.poke(inputBits)
-
+        //Fuzz loop
+        def fuzzLoop(curCoverage: Int, corpusIdx: BigInt) : Int = {
+            if((curCoverage == target) || (corpusIdx == timeout)) curCoverage
+            else {
+                //TODO: Start with T from seedCorpus and poke
+                //TODO: When corpusIdx >= seedCorpus.size start mutating with AFL
+                //TODO: Call CR.totalCoverage to update curCoverage
+                ???
+            }
+        }
+        fuzzLoop(0, 0)
     }
 
     /**
       * Generic poke that pokes each input individually by:
-      * - Mapping the array byte to the right size to fit each UInt input.
+      * - Mapping the bit string to the right size to fit each UInt input.
       * - Poke new value into input.
       * @param dut the DUT that we want to poke.
-      * @param value the byte array read from the test corpus.
-      * @tparam T the modue type of the dut.
+      * @param inputs the bit string read from the test corpus.
+      * @tparam T the module type of the dut.
       */
-    def poke[T <: MultiIOModule](dut: T, value: Array[Byte]): Unit = ???
+    def poke[T <: MultiIOModule](dut: T, ports: Seq[(String,Data)], inputs: String): Unit = {
+        /**
+          * Retrieve a wanted input from the set of inputs
+          */
+        def extractInput(input: String, inputSize: Int, startIdx: Int): BigInt =
+            BigInt(input.slice(startIdx, startIdx + inputSize), inputSize)
+
+        //Get port widths
+        val sizes = ports.map(_._2.getWidth)
+        val totalSize = sizes.sum
+        val testInputs = sizes.foldLeft((Seq[BigInt](), 0)) {
+            case ((result, index), size) =>
+                val in = extractInput(inputs, size, sizes.slice(0, index).sum)
+                (result :+ in, index + 1)
+        }._1
+
+        //Group tests into waves of inputs separated by a step
+        testInputs.grouped(ports.size).foreach (t => {
+            ports.zip(t.zip(sizes)).foreach {
+                case ((_, data), (test, size)) => data.poke(test.U(size))
+            }
+            dut.clock.step()
+        })
+    }
 }
