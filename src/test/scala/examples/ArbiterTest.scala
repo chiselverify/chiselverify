a8abc33076574ac281ea591a79e549cd5aa1d1c7
Martin Schoeberl <martin@jopdesign.com>
2021-12-29 11:58:40 +0100

    fix arbitration example

--- src/test/scala/examples/ArbiterTest.scala
+++ src/test/scala/examples/ArbiterTest.scala
@@ -25,7 +25,27 @@ class ArbiterTest extends FlatSpec with ChiselScalatestTester {
       dut.clock.step()
       dut.io.in(2).valid.poke(false.B)
       dut.clock.step(10)
+      // disable for now to avoid Travis issue.
       dut.io.out.bits.expect(2.U)
+    }
+  }
+
+  // TODO: finish the test
+  it should "be fair" in {
+    test(new Arbiter(5, UInt(16.W))).withAnnotations(Seq(WriteVcdAnnotation)) { dut =>
+      for (i <- 0 until 5) {
+        dut.io.in(i).valid.poke(true.B)
+        dut.io.in(i).bits.poke((i*100).U)
+      }
+      // println("Result should be " + List(0, 100, 200, 300, 400).sum)
+      dut.io.out.ready.poke(true.B)
+      dut.clock.step()
+      for (i <- 0 until 40) {
+        if (dut.io.out.valid.peek().litToBoolean) {
+          println(dut.io.out.bits.peek().litValue())
+        }
+        dut.clock.step()
+      }
     }
   }
 }
